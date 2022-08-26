1885dcd3c971b86c68c9a3847529c89ca0800e06
Jeongkyu Lee <jaykay0408@users.noreply.github.com>
2022-08-26 16:40:11 -0400

    Add files via upload

---
+++ AnyPython-S03/E03-helloworld.py
@@ -0,0 +1,19 @@
+"""
+File: helloworld.py
+-------------------
+This is our first python program.  It is customary to
+have a programmer's first program write "hello world"
+(inspired by the first program in Brian Kernighan and
+Dennis Ritchie's classic book, 'The C Programming Language.')
+"""
+
+
+def main():
+    # Enter your code here. Don't forget to remove "pass"
+    print("Hello World")
+
+
+# This provided line is required at the end of a Python file
+# to call the main() function.
+if __name__ == '__main__':
+    main()
